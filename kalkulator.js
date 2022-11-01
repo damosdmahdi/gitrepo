6e7b3ce4f4528c133175414ce6b949b24a762aaa
damosdmahdi <godrihidayat2@gmail.com>
2022-11-01 09:16:48 +0700

    menambahkan operator di kalkulator js

---
+++ kalkulator.js
@@ -0,0 +1,12 @@
+const number1 = 20;
+const number2 = 40;
+
+if (operator == "+") {
+  result = number1 + number2;
+} else if (operator == "-") {
+  result = number1 - number2;
+} else if (operator == "*") {
+  result = number1 * number2;
+} else {
+  result = number1 / number2;
+}
